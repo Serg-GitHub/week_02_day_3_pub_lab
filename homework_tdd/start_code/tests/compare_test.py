762d0193fd7ae5c276f990081129b413c3c4de7c
Sergio <sergio.eneide@protonmail.com>
2021-06-23 21:35:12 +0100

    end_code

---
+++ homework_tdd/start_code/tests/compare_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from src.compare import compare
+
+class TestCompare(unittest.TestCase):
+
+    def test_compare_4_4_returns_numbers_are_the_same(self):
+     self.assertEqual("numbers are the same", compare(4, 4))
+
